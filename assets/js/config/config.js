0a540bc226664ac4102de0131d3704867cd81424
jibril-a11y <jibrilmuksin@gmail.com>
2026-06-10 12:35:51 +0700

    Update config.js

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -7,9 +7,9 @@
   - anon public  -> SUPABASE_ANON_KEY  (kunci 'anon', aman dipakai di frontend)
 */
 
-export const SUPABASE_URL = "https://ytmfsxiyzpygydcahbmf.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://ytmfsxiyzpygydcahbmf.supabase.co";
 export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0bWZzeGl5enB5Z3lkY2FoYm1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNjQyNDQsImV4cCI6MjA5NjY0MDI0NH0.0k8FGvOBrg5OrTDIUT9q4SelGrgXtxEccxaYmn9XIWY";
+  "sb_publishable_DUNEaxmOVp27XvLwJkhhHw_AoRiL_pA";
 
 // Nama bucket penyimpanan (sesuai schema.sql).
 export const BUCKET_MODELS = "models";
